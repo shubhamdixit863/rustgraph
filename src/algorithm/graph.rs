22a36f714fb6bc977c1a40e0904054035dfda6e3
shubham <shubhamdixit863@gmail.com>
2024-04-21 23:17:22 +0530

    rust code
    Signed-off-by: shubham <shubhamdixit863@gmail.com>

--- src/algorithm/graph.rs
+++ src/algorithm/graph.rs
@@ -1,1 +1,42 @@
 // Graph implementation
+
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Graph <T>{
+    adjacent_list:HashMap<T,Vec<T>>
+
+}
+
+
+impl <T> Graph<T>
+where T: std::cmp::Eq+std::hash::Hash+Clone{
+
+    // Initiailize a new Graph
+
+   pub fn new ()->Self{
+         Graph{
+            adjacent_list:HashMap::new()
+        }
+    }
+
+
+    pub fn add_vertex(&mut self,vertex: User){
+
+        // firstly entry(vertex) gets called ,if the entry is vacant (ie the vertext doesnt exists in the map)
+        // or insert_ the value returned bty Vec::new()
+        // if the entry is occupied
+        self.adjacent_list.entry(vertex)
+            .or_insert(Vec::new());
+
+    }
+
+    pub fn add_edge(&mut self,src:T ,dst:T ){
+        if let Some(neighbors) =self.adjacent_list.get_mut(&src){
+            neighbors.push(dst.clone())
+        }else{
+            self.adjacent_list.insert(src.clone(),vec![dst.clone()]);
+        }
+        self.add_vertex(dst);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-mod algorithm;
+use graphproject::algorithm::graph::Graph;
+
+struct User{
+    name:String,
+    id:i32,
+
+}
 
 fn main() {
-    println!("Hello, world!");
+
+
+let mut graph = Graph::new();
+    graph.add_vertex("A");
+    graph.add_vertex("B");
+    graph.add_vertex("C");
+
+    graph.add_edge("A","B");
+    graph.add_edge("B","A");
+    graph.add_edge("C","A");
+    graph.add_edge("A","C");
+
+    // user database
+
+
+
+
+
+    println!("{:?}",graph);
+
 }
